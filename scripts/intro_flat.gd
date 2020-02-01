841337d774f74f8d7b7a798a0fcb6f45eaf8b6b6
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2020-02-01 17:16:35 +0100

    Added simple intro

---
+++ scripts/intro_flat.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _input(event):
+    if Input.is_key_pressed(KEY_ESCAPE):
+        get_tree().quit()
+
+func _input_event(viewport, event, shape_idx):
+    if event is InputEventMouseButton and event.pressed:
+        get_tree().change_scene("scenes/snafu.tscn")
