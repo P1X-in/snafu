48a507ede60a3922205aa897c49fe7a9f66ab20a
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2020-02-02 11:40:52 +0100

    Increased the disaster tick frequency 5x to make it more fluid

--- scripts/disaster.gd
+++ scripts/disaster.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export var growth_rate = 0.01
+export var growth_rate = 0.002
 export var extinguish_rate = 0.13
 export var lethality = 525000
 export var max_scale = 2.0
@@ -15,35 +15,35 @@ onready var world = $"../../../world"
 onready var click_player = $"click"
 
 func _ready():
-	self.size = self.nova.texture.get_size()
+    self.size = self.nova.texture.get_size()
 
 func _disaster_tick():
-	if not self.stopped:
-		self.disaster_scale += self.growth_rate
+    if not self.stopped:
+        self.disaster_scale += self.growth_rate
 
-		if self.disaster_scale > self.max_scale:
-			self.disaster_scale = self.max_scale
+        if self.disaster_scale > self.max_scale:
+            self.disaster_scale = self.max_scale
 
-		self._update_nova_size()
-		self.world.kill_people(int(self.disaster_scale * self.disaster_scale * self.lethality))
-	else:
-		self.queue_free()
+        self._update_nova_size()
+        self.world.kill_people(int(self.disaster_scale * self.disaster_scale * self.lethality))
+    else:
+        self.queue_free()
 
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
-		self.click_player.play()
-		self.disaster_scale -= self.extinguish_rate
+    if event is InputEventMouseButton and event.pressed:
+        self.click_player.play()
+        self.disaster_scale -= self.extinguish_rate
 
-		if self.disaster_scale < 0.0 and not self.stopped:
-			self.disaster_scale = 0.01
-			self.stopped = true
+        if self.disaster_scale < 0.0 and not self.stopped:
+            self.disaster_scale = 0.01
+            self.stopped = true
 
-			self.world.extinguished()
-			self.world.add_score(100)
-		else:
-			self.world.add_score(15)
+            self.world.extinguished()
+            self.world.add_score(100)
+        else:
+            self.world.add_score(15)
 
-		self._update_nova_size()
+        self._update_nova_size()
 
 func _update_nova_size():
-	self.nova.rect_scale = Vector2(1.0, 1.0) * self.disaster_scale
+    self.nova.rect_scale = Vector2(1.0, 1.0) * self.disaster_scale
